472a9c8771a4bf6045737b58fd3e2032dc5e5654
이명열 <dandy83ten@gmail.com>
2026-04-07 00:43:57 +0900

    feat(player):캐릭터 및 플레이어 정보 출력 메소드 추가

--- Models/Character.cs
+++ Models/Character.cs
@@ -20,7 +20,6 @@ public class Character
     #endregion
 
     #region 생성자
-
     protected Character(string name, int maxHp, int maxMp, int attackPower, int defense, int level)
     {
         Name = name;
@@ -33,13 +32,22 @@ public class Character
         Level = level;
         
     }
-    
-
     #endregion
 
     #region 메서드
-
+    // 공통으로 사용할 메소드들
     
+    // 캐릭터 스탯 출력
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"==== {Name} 정보 ====");
+        Console.WriteLine($"레벨: {Level}");
+        Console.WriteLine($"체력: {CurrentHP}/{MaxHP}");
+        Console.WriteLine($"마나: {CurrentMP}/{MaxMP}");
+        Console.WriteLine($"공격력: {AttackPower}");
+        Console.WriteLine($"방어력: {Defense}");
+        Console.WriteLine("=======================");
+    }
 
     #endregion
 }
